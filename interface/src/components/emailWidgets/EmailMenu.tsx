ba91e8d482b6bdb89fd3c1b92ac56c2b4ebbe62c
Abubakar Balogun <abzynobalogun@gmail.com>
2024-06-26 14:01:11 +0100

    Resolve issues related to incorrect import path

--- interface/src/components/emailWidgets/EmailMenu.tsx
+++ interface/src/components/emailWidgets/EmailMenu.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import EmailList from "./EmailList";
 import EmailOptions from "./EmailOptions";
-import { EmailMenuProps } from "./types";
+import { EmailMenuProps } from "./../../types";
 
 const EmailMenu: React.FC<EmailMenuProps> = ({ emails, allSelected, onSelectAll, onSelectEmail, onStarEmail }) => {
   return (
-    <section className="border rounded-tr-lg rounded-tl-lg overflow-hidden border-gray-300 dark:border-neutral-800">
+    <section className="overflow-hidden border border-gray-300 rounded-tl-lg rounded-tr-lg dark:border-neutral-800">
       <EmailOptions onSelectAll={onSelectAll} allSelected={allSelected} />
       <div className="">
         {emails.map((email) => (
