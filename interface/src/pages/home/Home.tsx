5772866924801a43660e1bb47918faaa9f7884cd
Abubakar Balogun <abzynobalogun@gmail.com>
2024-06-26 12:27:29 +0100

    Add Homepage

---
+++ interface/src/pages/home/Home.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import EmailView from "../../components/EmailView/EmailView";
+import Sidebar from "../../components/ui/sidebar/Sidebar";
+
+const Home = () => {
+  const [isComposeEmailOpen, setIsComposeEmailOpen] = useState(false);
+
+  const handleOpenDialog = () => {
+    setIsComposeEmailOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsComposeEmailOpen(false);
+  };
+  return (
+    <main className="flex">
+      <Sidebar onComposeEmail={handleOpenDialog} />
+      <EmailView
+        isDialogOpen={isComposeEmailOpen}
+        onDialogClose={handleCloseDialog}
+      />
+    </main>
+  );
+};
+
+export default Home;
