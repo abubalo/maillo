ca518d475963d9d7a8638707fd41e2ea4457f60b
Abubakar Balogun <abzynobalogun@gmail.com>
2024-06-26 13:44:49 +0100

    Create sidebar item component

---
+++ interface/src/components/ui/sidebar/SidebarItem.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+type Props = {
+  icon: ReactNode;
+  text: string;
+  isCollapsed: boolean;
+  className?: string;
+};
+
+const SidebarItem = ({ icon, text, isCollapsed, className }: Props) => {
+  return (
+    <Link
+      to={`?view=${text.toLowerCase()}`}
+      className={twMerge(
+        "flex items-center transition-all duration-300 rounded-md hover:bg-neutral-800/50",
+        className
+      )}
+    >
+      <span className="p-4 text-xl">{icon}</span>
+      <motion.span
+        initial={false}
+        animate={{ opacity: isCollapsed ? 0 : 1, width: isCollapsed ? 0 : 'auto' }}
+        transition={{ duration: 0.1, ease: "linear" }}
+        className="flex font-semibold overflow-hidden whitespace-nowrap"
+      >
+        {text}
+      </motion.span>
+    </Link>
+  );
+};
+
+export default SidebarItem;
