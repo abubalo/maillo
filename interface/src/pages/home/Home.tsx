4c76aceb9866a87efebb5180272e412f8d0e500a
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-10 12:01:56 +0200

    refactor: Remove the props

--- interface/src/pages/home/Home.tsx
+++ interface/src/pages/home/Home.tsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
 import EmailView from "../../components/emailWidgets/EmailView";
 import Sidebar from "../../components/ui/sidebar/Sidebar";
 
 const Home = () => {
-  const [isComposeEmailOpen, setIsComposeEmailOpen] = useState(false);
-
-  const handleOpenDialog = () => {
-    setIsComposeEmailOpen(true);
-  };
-
-  const handleCloseDialog = () => {
-    setIsComposeEmailOpen(false);
-  };
   return (
     <main className="flex">
-      <Sidebar onComposeEmail={handleOpenDialog} />
-      <EmailView
-        isDialogOpen={isComposeEmailOpen}
-        onDialogClose={handleCloseDialog}
-      />
+      <Sidebar/>
+      <EmailView />
     </main>
   );
 };
