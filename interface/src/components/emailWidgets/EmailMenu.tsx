575f70734c9227e7d3d85b7e3b470ffef5adb006
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-10 18:08:20 +0200

    chore: Formatted the code for readability

--- interface/src/components/emailWidgets/EmailMenu.tsx
+++ interface/src/components/emailWidgets/EmailMenu.tsx
@@ -2,13 +2,33 @@ import EmailList from "./EmailList";
 import EmailOptions from "./EmailOptions";
 import { EmailMenuProps } from "./../../types";
 
-const EmailMenu: React.FC<EmailMenuProps> = ({ emails, allSelected, onSelectAll, onSelectEmail, onStarEmail }) => {
+const EmailMenu: React.FC<EmailMenuProps> = ({
+  emails,
+  allSelected,
+  onSelectAll,
+  onSelectEmail,
+  onStarEmail,
+  onDeleteEmail,
+  onMarkAsRead,
+  onMarkAsUnread,
+  onArchiveEmail,
+  onMarkAsSpam,
+}) => {
+
+  
   return (
     <section className="overflow-hidden border border-gray-300 rounded-tl-lg rounded-tr-lg dark:border-neutral-800">
       <EmailOptions onSelectAll={onSelectAll} allSelected={allSelected} />
       <div className="">
         {emails.map((email) => (
-          <EmailList key={email.id} {...email} onSelect={onSelectEmail} onStar={onStarEmail} />
+          <EmailList
+            key={email.id}
+            {...email}
+            onSelect={onSelectEmail}
+            onStar={onStarEmail}
+            onDelete={onDeleteEmail}
+            {...{onSelectEmail, onStarEmail, onDeleteEmail, onMarkAsRead, onMarkAsUnread, onArchiveEmail, onMarkAsSpam }}
+          />
         ))}
       </div>
     </section>
