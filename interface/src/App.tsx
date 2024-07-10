2aaa58288d78f3be14191b002aa1900c4851e6c4
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-10 12:09:43 +0200

    Update default page path

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Inbox from "./components/Inbox";
 import Signup from "./pages/signup/Signup";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -8,8 +7,7 @@ function App() {
   return (
       <Routes>
         <Route path="/" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-        <Route index path="home/view" element={<Inbox />} />
+        <Route path="/home#inbox" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
   );
