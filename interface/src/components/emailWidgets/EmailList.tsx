770e80167d96ed8bd3f8531b2b09eff625b83e81
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-10 17:36:18 +0200

    refactor: Remove unused variables

--- interface/src/components/emailWidgets/EmailList.tsx
+++ interface/src/components/emailWidgets/EmailList.tsx
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom";
-import { useSearchParams } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { StarIcon, StarSolidIcon } from "../shared/Icons";
-import { Email } from "../types";
+import { Email } from "./../../types";
 
 interface EmailListProps extends Email {
   onSelect: (id: string | number) => void;
   onStar: (id: string | number) => void;
+  onDelete: (id: string | number) => void;
 }
 
 const EmailList: React.FC<EmailListProps> = ({
@@ -15,12 +15,19 @@ const EmailList: React.FC<EmailListProps> = ({
   preview,
   timestamp,
   isUnread,
-  unStared,
+  isStarred,
   isSelected,
   onSelect,
   onStar,
-  link,
+  detailUrl,
+  onDelete,
+  onMarkAsRead,
+  onMarkAsUnread,
+  onArchiveEmail,
+  onMarkAsSpam,
 }) => {
+  const { hash } = useLocation();
+
   const handleCheckboxChange = () => {
     onSelect(id);
   };
@@ -31,36 +38,39 @@ const EmailList: React.FC<EmailListProps> = ({
     onStar(id);
   };
 
-  const [searchParams] = useSearchParams();
-  const activeLink = searchParams.get("view");
+  const fullLink = `/home/${hash}/${detailUrl}`;
 
-  const links = ["inbox", "drafts", "spam", "sent", "bin"]
-
-  // console.log(links[activeLink]);
-  // console.log(activeLink);
+  const truncateText =(length = 10) =>{
+// Ten words not characters
+preview.split(" ")
+  }
 
   return (
-    <NavLink
-      to={`/${link}`}
-      className={({ isActive }) =>
-        `flex items-center space-x-4 p-3 border-b cursor-pointer border-gray-300 dark:border-neutral-800 ${
-          isUnread ? "font-bold bg-neutral-50 dark:bg-neutral-800/30" : ""
-        } ${isActive ? "text-blue-600" : ""}`
-      }
+    <Link
+      to={`${fullLink}`}
+      className={`flex items-center space-x-4 p-3 border-b cursor-pointer border-gray-300 dark:border-neutral-800 ${
+        isUnread
+          ? "font-bold bg-neutral-50 dark:bg-neutral-800/40"
+          : "font-semibold"
+      } `}
     >
+
+      <div>
+        
+      </div>
       <div className="flex items-center space-x-2">
         <div className="flex items-center" onClick={(e) => e.stopPropagation()}>
           <label className="inline-flex items-center">
             <input
               type="checkbox"
-              className="form-checkbox h-5 w-5 text-blue-600"
+              className="w-5 h-5 text-blue-600 form-checkbox"
               checked={isSelected}
               onChange={handleCheckboxChange}
             />
           </label>
         </div>
         <div onClick={handleStarEmail}>
-          {unStared ? <StarSolidIcon /> : <StarIcon />}
+          {isStarred ? <StarSolidIcon className="text-yellow-500"/> : <StarIcon />}
         </div>
       </div>
       <div className="w-1/4 truncate">{sender}</div>
@@ -68,8 +78,8 @@ const EmailList: React.FC<EmailListProps> = ({
         <div className="truncate">{subject}</div>
         <div className="text-sm text-gray-600 truncate">{preview}</div>
       </div>
-      <div className="w-1/4 text-right text-sm text-gray-500">{timestamp}</div>
-    </NavLink>
+      <div className="w-1/4 text-sm text-right text-gray-500">{timestamp}</div>
+    </Link>
   );
 };
 
