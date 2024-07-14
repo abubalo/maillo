ef6990e0d4b7ba64ba9e4e7c905806c6a94c1f2e
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-14 15:48:04 +0200

    Fix issues that is preventing some file extension properly

--- interface/src/utils/getFileIcon.tsx
+++ interface/src/utils/getFileIcon.tsx
@@ -1,10 +1,6 @@
-import {
-    MdImage,
-    MdOutlinePictureAsPdf
-  } from "react-icons/md";
+import { MdImage, MdOutlinePictureAsPdf } from "react-icons/md";
 import { GoogleDocIcon, PowerPointIcon } from "../components/shared/Icons";
 import { FaPaperclip } from "react-icons/fa";
-
 
 export const getFileIcon = (type: string) => {
   switch (type) {
@@ -12,15 +8,16 @@ export const getFileIcon = (type: string) => {
       return <MdOutlinePictureAsPdf className="text-red-500" size={24} />;
     case "application/docx":
     case "application/doc":
-      return <GoogleDocIcon className="text-red-500" size={24} />;
+    case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+      return <GoogleDocIcon className="text-blue-500" size={24} />;
     case "application/pptx":
     case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
       return <PowerPointIcon className="text-red-500" size={24} />;
     case "image/jpeg":
     case "image/png":
     case "image/gif":
-      return <MdImage className="text-blue-500" size={24} />;
+      return <MdImage className="text-green-600" size={24} />;
     default:
-      return <FaPaperclip className="mr-2" />;
+      return <FaPaperclip  className="mr-2 text-purple-600" />;
   }
 };
