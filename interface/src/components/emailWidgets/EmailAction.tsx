dd132094886aa6b1d0fb7c85b43731d7f48b0906
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-14 15:39:49 +0200

    Add go back home icon to EmailAction component

--- interface/src/components/emailWidgets/EmailAction.tsx
+++ interface/src/components/emailWidgets/EmailAction.tsx
@@ -2,33 +2,41 @@ import Tooltip from "../ui/tooltip/Tooltip";
 import {
   ArchiveIcon,
   DeleteIcon,
+  GoBackIcon,
   SpamIcon,
 } from "../shared/Icons";
 import { MdMarkAsUnread } from "react-icons/md";
 import { useEmailStoreState } from "../../stores/stateStores";
 import { Email } from "../../types";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const EmailActions: React.FC<{ email: Email }> = ({ email }) => {
+  const navigate = useNavigate();
+  
+
+  const handleGoBack = () =>{
+    navigate(-1)
+  }
   const {
     handleArchiveEmail,
     handleMarkAsRead,
     handleMarkAsSpamEmail,
     handleDeleteEmail,
-    handleStarEmail,
-    handleReply,
   } = useEmailStoreState();
 
-  const onStar = () => handleStarEmail(email.id);
+  
   const onArchive = () => handleArchiveEmail(email.id);
   const onDelete = () => handleDeleteEmail(email.id);
   const onMarkAsRead = () => handleMarkAsRead(email.id);
   const onMarkAsSpam = () => handleMarkAsSpamEmail(email.id);
-  const onReply = () => handleReply();
 
   return (
     <div className="flex items-center justify-between p-4 rounded-tl-md rounded-tr-md bg-neutral-500/40">
-      <div className="flex items-center gap-3 text-lg">
+      <div className="flex items-center gap-6 text-lg">
+        <TooltipButton text="Back to Inbox" onClick={handleGoBack}>
+          <GoBackIcon />
+        </TooltipButton>
         <TooltipButton text="Archive" onClick={onArchive}>
           <ArchiveIcon />
         </TooltipButton>
