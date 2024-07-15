672bd3cedecdf5ebb222208e980b12250ba28b6a
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-15 20:01:41 +0200

    Improve 404 component

--- interface/src/components/errors/NotFound.tsx
+++ interface/src/components/errors/NotFound.tsx
@@ -1,9 +1,15 @@
+import EmptyItem from "../shared/EmptyItem";
+
 type Props = {
   message?: string;
 };
 
 const NotFound: React.FC<Props> = ({ message = "Page not found" }) => {
-  return <div>{message}</div>;
+  return (
+    <div>
+      <EmptyItem message={message} />
+    </div>
+  );
 };
 
 export default NotFound;
