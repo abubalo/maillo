ebb96bf5988eb866599ca60ea04fc3eddf4421c8
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-15 19:14:39 +0200

    Improve the component by adding image and discriptive text

--- interface/src/components/shared/EmptyItem.tsx
+++ interface/src/components/shared/EmptyItem.tsx
@@ -1,5 +1,25 @@
-const EmptyItem = () => {
-  return <div>Nothing to see here!</div>;
+// import cuteAlien from "../../assets/cute-alien.png";
+// import cuteHoneybee from "../../assets/cute-honeybee.png";
+import cuteSleepingCat from "../../assets/cute-sleeping-cat.png";
+
+type Props = {
+  url?: string;
+  message?: string;
+};
+
+const EmptyItem: React.FC<Props> = ({
+  url = cuteSleepingCat,
+  message = "No Items",
+}) => {
+  
+  return (
+    <section className="flex items-center justify-center w-full h-svh">
+      <div className="flex flex-col items-center justify-center ">
+        <img src={url} alt="" className="w-64 h-auto mx-auto borde" />
+        <h2 className="text-2xl">{message}</h2>
+      </div>
+    </section>
+  );
 };
 
 export default EmptyItem;
