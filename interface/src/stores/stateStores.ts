bd9634c090cf805f10267b3548ef9157674b2fbf
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-16 15:31:40 +0200

    Add advance search option  store

--- interface/src/stores/stateStores.ts
+++ interface/src/stores/stateStores.ts
@@ -54,6 +54,29 @@ export interface EmailStoreState {
   handleArchiveEmail: (id: string | number) => void;
 }
 
+type AdvanceOptiosStore = {
+  from: string;
+  to: string;
+  subject: string;
+  hasWords: string;
+  doesntHave: string;
+  size: string;
+  byteOption: number | string;
+  dateWithin: string | number;
+  hasAttachment: "yes" | "no";
+  doesntIncludeChats: "yes" | "no";
+  setFrom: (from: string) => void;
+  setTo: (to: string) => void;
+  setSubject: (subject: string) => void;
+  setHasWords: (hasWords: string) => void;
+  setDoesntHave: (doesntHave: string) => void;
+  setSize: (size: string) => void;
+  setByteOption: (byteOption: string | number) => void;
+  setDateWithin: (dateWith: string | number) => void;
+  setHasAttachment: (hasAttachment: "yes" | "no") => void;
+  setDoesntIncludeChats: (doesntIncludeChats: "yes" | "no") => void;
+};
+
 export const useDialogStore = create<DialogState>((set) => ({
   isComposeEmailOpen: false,
   openComposeEmail: () => set({ isComposeEmailOpen: true }),
@@ -77,6 +100,29 @@ export const useComposeEmailStore = create<ComposeEmailState>((set) => ({
       attachments: state.attachments.filter((_, i) => i !== index),
     })),
   clearAttachments: () => set({ attachments: [] }),
+}));
+
+export const useAdvanceSearchStore = create<AdvanceOptiosStore>((set) => ({
+  from: "",
+  to: "",
+  subject: "",
+  hasWords: "",
+  doesntHave: "",
+  size: "",
+  byteOption: "MB",
+  dateWithin: "",
+  hasAttachment: "no",
+  doesntIncludeChats: "yes",
+  setFrom: (from) => set({ from }),
+  setTo: (to) => set({ to }),
+  setSubject: (subject) => set({ subject }),
+  setHasWords: (hasWords) => set({ hasWords }),
+  setDoesntHave: (doesntHave) => set({ doesntHave }),
+  setSize: (size) => set({ size }),
+  setByteOption: (byteOption) => set({byteOption}),
+  setDateWithin: (dateWithin) => set({ dateWithin }),
+  setHasAttachment: (hasAttachment) => set({ hasAttachment }),
+  setDoesntIncludeChats: (doesntIncludeChats) => set({ doesntIncludeChats }),
 }));
 
 export const useEmailStoreState = create<EmailStoreState>((set) => ({
