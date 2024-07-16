a5a260165effcbb2541147d453847b71d44dadfe
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-16 13:53:16 +0200

    Made changes to default styles

--- interface/src/components/ui/button/Button.tsx
+++ interface/src/components/ui/button/Button.tsx
@@ -4,17 +4,23 @@ import { twMerge } from "tailwind-merge";
 type Props = {
   children?: ReactNode;
   className?: string;
+  arialLabel?: string;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<Props> = ({
   children = "Button",
   className,
+  arialLabel,
   ...rest
 }) => {
   return (
     <button
-      className={twMerge("w-full  p-4 bg-blue-600 text-white", className)}
+      className={twMerge(
+        "w-full  px-4 py-2 bg-blue-600 text-white text-nowrap",
+        className
+      )}
       {...rest}
+      aria-label={arialLabel}
     >
       {children}
     </button>
