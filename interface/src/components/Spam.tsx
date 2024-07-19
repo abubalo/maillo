374ce3e79f85fe7990b9670adb9e8f43c3a6e742
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-19 23:44:16 +0200

    Remove unneccasry props that is causing maintainer isue

--- interface/src/components/Spam.tsx
+++ interface/src/components/Spam.tsx
@@ -1,11 +1,10 @@
 import EmailMenu from "./emailWidgets/EmailMenu";
-import { EmailMenuProps } from "../types";
+import { Email } from "../types";
 
-type Props = EmailMenuProps;
-const Spam: React.FC<Props> = ({ emails, allSelected }) => {
+const Spam: React.FC<{ emails: Email[] }> = ({ emails }) => {
   return (
     <div>
-      <EmailMenu {...{ emails, allSelected }} />
+      <EmailMenu emails={emails}/>
     </div>
   );
 };
