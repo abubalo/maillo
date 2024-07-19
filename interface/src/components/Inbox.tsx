f0152a45704ac5abfad895fee73801fbb22e8835
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-19 23:43:47 +0200

    Remove unneccasry props that is causing maintainer issue

--- interface/src/components/Inbox.tsx
+++ interface/src/components/Inbox.tsx
@@ -1,11 +1,10 @@
 import EmailMenu from "./emailWidgets/EmailMenu";
-import { EmailMenuProps } from "../types";
+import { Email } from "../types";
 
-type Props = EmailMenuProps;
-const Inbox: React.FC<Props> = ({ emails, allSelected }) => {
+const Inbox: React.FC<{ emails: Email[] }> = ({ emails }) => {
   return (
     <div>
-      <EmailMenu {...{ emails, allSelected }} />
+      <EmailMenu emails={emails}/>
     </div>
   );
 };
