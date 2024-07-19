0c1789918a24b8f22f1ad52ec9286b62a4e40369
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-19 23:56:01 +0200

    Fix issues leading to rendering error and preventing states from executing properly

--- interface/src/components/emailWidgets/EmailListOptions.tsx
+++ interface/src/components/emailWidgets/EmailListOptions.tsx
@@ -11,27 +11,37 @@ import ToolTip from "../ui/tooltip/Tooltip";
 type Props = {
   id: string;
   isUnread: boolean;
+  isDeleted: boolean;
 };
 
-const EmailListOptions: React.FC<Props> = ({ id, isUnread }) => {
+const EmailListOptions: React.FC<Props> = ({ id, isUnread, isDeleted }) => {
   const {
-    handleArchiveEmail,
-    handleDeleteEmail,
-    handleMarkAsRead,
-    handleMarkAsUnread,
+    onArchive,
+    onDeleteEmails,
+    onMarkAsRead,
+    onMarkAsUnread,
+    onPermanentDelete,
   } = useEmailStoreState();
 
-  const handleClick = (e: React.MouseEvent, action: (id: string) => void) => {
+  const handleClick = (
+    e: React.MouseEvent,
+    action: ((ids: string[]) => void) | ((id: string) => void),
+    id: string
+  ) => {
     e.preventDefault();
     e.stopPropagation();
-    action(id);
+    if (action.length === 1) {
+      (action as (id: string) => void)(id);
+    } else {
+      (action as (ids: string[]) => void)([id]);
+    }
   };
 
   return (
-    <div className="flex items-center justify-center space-x-1 text-lg text-black bg-white border rounded-md w-max h-max dark:bg-black dark:text-white">
+    <div className="flex items-center justify-center space-x-1 text-lg rounded-md w-max dark:bg-inherit dark:text-white">
       <button
         type="button"
-        onClick={(e) => handleClick(e, handleArchiveEmail)}
+        onClick={(e) => handleClick(e, onArchive, id)}
         className="p-2 text-lg"
         aria-label="Archive"
       >
@@ -41,24 +51,32 @@ const EmailListOptions: React.FC<Props> = ({ id, isUnread }) => {
       </button>
       <button
         type="button"
-        onClick={(e) => handleClick(e, handleDeleteEmail)}
+        onClick={(e) =>
+          handleClick(e, isDeleted ? onPermanentDelete : onDeleteEmails, id)
+        }
         className="p-2 text-lg"
         aria-label="Delete"
       >
-        <ToolTip text="Delete">
-          <DeleteIcon size={20} />
-        </ToolTip>
+        {isDeleted ? (
+          <ToolTip text="Delete Permanently">
+            <DeleteIcon size={20} />
+          </ToolTip>
+        ) : (
+          <ToolTip text="Delete">
+            <DeleteIcon size={20} />
+          </ToolTip>
+        )}
       </button>
       <button
         type="button"
         onClick={(e) =>
-          handleClick(e, isUnread ? handleMarkAsRead : handleMarkAsUnread)
+          handleClick(e, isUnread ? onMarkAsRead : onMarkAsUnread, id)
         }
         className="p-2 text-lg"
         aria-label={isUnread ? "Mark as Read" : "Mark as Unread"}
       >
         <ToolTip text={isUnread ? "Mark as Read" : "Mark as Unread"}>
-          {isUnread ? <MakrUnreadIcon size={20} /> : <MarkReadIcon size={20} />}
+          {isUnread ? <MarkReadIcon size={20} /> : <MakrUnreadIcon size={20} />}
         </ToolTip>
       </button>
       <button type="button" aria-label="Snooze" className="p-2 text-lg">
