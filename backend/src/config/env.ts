6ed1324521e4be391ea4cc628fa34fc03b449fe7
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-24 09:46:26 +0200

    Add new env fields

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,8 +1,13 @@
-import { cleanEnv, str } from "envalid";
+import { cleanEnv, num, str } from "envalid";
 
 export const env = cleanEnv(process.env, {
   PORT: str(),
   DEVECOT_USER: str(),
   DEVECOT_PASSWORD: str(),
   MONGODB_URL: str(),
+  SMPT_HOST: str(),
+  SMPT_PORT: num(),
+  SMPT_SERVICE: str(),
+  SMTP_SENDER_ADDRESS: str(),
+  SMTP_SENDER_PASSWORD: str(),
 });
