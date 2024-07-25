7e9f3a4e5854858c8789bbdd8a5ddd8ec4204ad6
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-25 10:51:21 +0200

    Create model for user storage management

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: false },
+  profileImage: { type: String, required: false },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  lastLoggedin: { type: Date, default: Date.now },
+  isActive: { type: Boolean, default: true },
+  role: { type: String, default: "regular" },
+  settings: {
+    theme: { type: String, default: "light" },
+    notifications: { type: Boolean, default: true },
+  },
+});
+
+userSchema.pre("save", function (next) {
+  this.updatedAt = new Date();
+  next();
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
