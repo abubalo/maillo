222b9913de04ef55eb9436b4417f2f9eddd79734
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-25 10:20:04 +0200

    Set up cors options

---
+++ backend/src/config/corsOptions.ts
@@ -0,0 +1,20 @@
+import { CorsOptions } from 'cors';
+
+export const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins: string[] = [
+      'http://localhost:5371',
+      'https://uokpq.vercel.app',
+      'https://www.uokpq.com',
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  maxAge: 600, // 10 minutes
+};
