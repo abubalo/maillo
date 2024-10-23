a861290868e1c9dccf28ba1378c676b51461c908
Abubakar Balogun <abzynobalogun@gmail.com>
2024-10-23 14:53:57 +0200

    Add uploader route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,18 +2,27 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import compression from "compression";
-import { HttpLogger } from "./middleware/httpLogger";
-import { authLimiter } from "./middleware/limiter";
+import httpLogger from "./middleware/httpLogger";
+import { apiRateLimiter, authLimiter } from "./middleware/limiter";
+import { corsOptions } from "./config/corsOptions";
+import userRoutes from "./routes/user.routes";
+import emailRoutes from "./routes/email.routes";
+import { isAuthenticated } from "./middleware/isAuthenticated";
+import { isAuthorized } from "./middleware/isAuthorized";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(compression());
-
-app.use(HttpLogger);
+app.use(httpLogger);
 app.use(authLimiter);
+app.use(errorHandler);
+
+app.use("users/", apiRateLimiter, userRoutes);
+app.use("maillo/", apiRateLimiter, isAuthenticated, isAuthorized, emailRoutes);
 
 export default app;
