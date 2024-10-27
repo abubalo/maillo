eef4b469d57fe27755ddb6beaee1a445663bd48a
Abubakar Balogun <abzynobalogun@gmail.com>
2024-10-27 15:31:56 +0200

    Update .env validator

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,13 +1,20 @@
 import { cleanEnv, num, str } from "envalid";
+import {config} from "dotenv"
+
+config();
 
 export const env = cleanEnv(process.env, {
   PORT: str(),
-  DEVECOT_USER: str(),
-  DEVECOT_PASSWORD: str(),
+  JWT_SECRET: str(),
   MONGODB_URL: str(),
   SMPT_HOST: str(),
   SMPT_PORT: num(),
   SMPT_SERVICE: str(),
   SMTP_SENDER_ADDRESS: str(),
   SMTP_SENDER_PASSWORD: str(),
+  MAX_POOL_SIZE: num(),
+  MIN_POOL_SIZE: num(),
+  AQUIRED_TIMEOUT: num(),
+  IDLE_TIMEOUT: num(),
+  SESSION_MAX_AGE: num(),
 });
