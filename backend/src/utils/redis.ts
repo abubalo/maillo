f0b128acad26f9add3822de3c4b479b28e57f23d
Abubakar Balogun <abzynobalogun@gmail.com>
2024-10-30 10:20:19 +0200

    Update redis config

--- backend/src/utils/redis.ts
+++ backend/src/utils/redis.ts
@@ -1,11 +1,11 @@
-import Redis from 'ioredis';
-import { redisConfig } from '@config/redis.config';
+import { env } from "@/config/env";
+import Redis from "ioredis";
 
 let redisClient: Redis | null = null;
 
 export function getRedisClient(): Redis {
   if (!redisClient) {
-    redisClient = new Redis(redisConfig);
+    redisClient = new Redis(env.REDIS_UPSTACH_URL || "");
   }
   return redisClient;
 }
