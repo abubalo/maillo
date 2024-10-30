c929171db7a7d53bcf926c912e4a03d8f6dd1435
Abubakar Balogun <abzynobalogun@gmail.com>
2024-10-30 18:51:58 +0200

    fix missing relative path

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -5,9 +5,11 @@ import { isAuthorized } from "@/middleware/isAuthorized";
 
 const router = Router();
 
-router.post("create", userController.addUser)
-router.post("login", userController.loginUser)
-router.get("user:id", isAuthenticated, isAuthorized, userController.getUserById)
-router.get("users", isAuthenticated, isAuthorized, userController.getUsers)
-router.put("update:id", isAuthenticated, isAuthorized, userController.updateUser)
-router.delete("delete:id", isAuthenticated, isAuthorized, userController.deleteUser)
+router.post("/create", userController.addUser)
+router.post("/login", userController.loginUser)
+router.get("/user:id", isAuthenticated, isAuthorized, userController.getUserById)
+router.put("/update:id", isAuthenticated, isAuthorized, userController.updateUser)
+router.delete("/delete:id", isAuthenticated, isAuthorized, userController.deleteUser)
+
+const userRoutes = router;
+export default userRoutes;
