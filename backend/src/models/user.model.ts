53950dab65f7d01a5982c8387151598c7e6dfaaa
Abubakar Balogun <abzynobalogun@gmail.com>
2024-10-30 19:01:30 +0200

    Set lastName required feild to true

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -5,7 +5,7 @@ export const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
-  lastName: { type: String, required: false },
+  lastName: { type: String, required: true },
   profileImage: { type: String, required: false },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
